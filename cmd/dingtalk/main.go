8ae23e55fca33b453555325f69944866b40e9c22
aumujun <aumujun@gmail.com>
2019-10-28 23:36:37 +0800

    .

--- cmd/dingtalk/main.go
+++ cmd/dingtalk/main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	dingtalk "peeka/cmd/dingtalk/core"
+)
+
 func main() {
+	l := new(dingtalk.ListRecord)
+	err := l.GetListRecordDetails([]string{"156339501623369564"}, "2019-10-28 08:00:00", "2019-10-29 00:00:00")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(l)
 }
