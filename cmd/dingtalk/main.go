6970f99429aabc0de09e887860dbe55a0067eb26
aumujun <aumujun@gmail.com>
2019-10-28 19:56:17 +0800

    nothing

--- cmd/dingtalk/main.go
+++ cmd/dingtalk/main.go
@@ -1,13 +1,4 @@
 package main
 
-import (
-	"fmt"
-	dingtalk "peeka/cmd/dingtalk/core"
-)
-
 func main() {
-	// listschedule := new(dingtalk.ListSchedule)
-	// _ = listschedule.GetScheduleList("2019-10-28", 0, 200)
-	fmt.Println(dingtalk.APPKEY)
-
 }
