2055f1ed020122a1da38f780f77ee747456f4ced
aumujun <aumujun@gmail.com>
2019-11-01 18:56:16 +0800

    删除添加了一些功能

--- cmd/screenshot/main.go
+++ cmd/screenshot/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"peeka/internal/screenshot/run"
 )
 
 func main() {
-	run.Run()
+	urls := run.Run()
+	var buffer bytes.Buffer
+	buffer.WriteString("## 定时网络巡检播报!\n\n\n")
+	for k, v := range urls {
+		buffer.WriteString("**" + k + "**\n\n\n")
+		buffer.WriteString("![" + k + "]" + "(" + v + ")\n")
+	}
+	// chatbot.Run("ALL", buffer.String())
 }
--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/chromedp/chromedp v0.5.1
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/ozankasikci/go-image-merge v0.2.0
+	github.com/qiniu/api.v7/v7 v7.4.0
 )
