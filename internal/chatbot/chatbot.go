f25cab5aa47b864d97663c9a21abdc5c18c58827
aumujun <aumujun@gmail.com>
2019-11-01 22:02:04 +0800

    move chatbot to internal

---
+++ internal/chatbot/chatbot.go
@@ -0,0 +1,59 @@
+package chatbot
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var (
+	baseURL = `https://oapi.dingtalk.com/robot/send?access_token=` + os.Getenv("ROBOT_TOKEN")
+)
+
+func Run(user, text string) {
+	fileName := time.Now().Format("20060102") + ".dingding.log"
+	logFile, _ := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
+	defer logFile.Close()
+	Log := log.New(logFile, "[Info]", log.Ldate|log.Ltime) // log.Ldate|log.Ltime|log.Lshortfile
+	Log.Println("开始发送消息!")
+	SendMsg(user, text, Log)
+}
+
+//发送消息到钉钉
+func SendMsg(user, text string, Log *log.Logger) {
+	jsonstring := `{
+     "msgtype": "markdown",
+     "markdown": {"title":"皮卡丘!",
+"text":"` + text + `"
+     },
+    "at": {
+        "atMobiles": [
+            "` + user + `"
+        ], 
+        "isAtAll": true
+    }
+ }`
+	reader := bytes.NewReader([]byte(jsonstring))
+	resp := Post(baseURL, reader)
+	Log.SetPrefix("[Info]")
+	Log.Printf("消息发送完成,服务器返回内容：%s", string(resp))
+}
+
+func Post(url string, reader *bytes.Reader) []byte {
+	request, err := http.NewRequest("POST", url, reader)
+	if err != nil {
+
+	}
+	request.Header.Set("Content-Type", "application/json")
+	client := http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer resp.Body.Close()
+	respBytes, _ := ioutil.ReadAll(resp.Body)
+	return respBytes
+}
