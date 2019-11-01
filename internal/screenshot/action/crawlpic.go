55a9f46302c2cf4880e45b63c0c338ef9c3c66c6
aumujun <aumujun@gmail.com>
2019-11-01 18:52:55 +0800

    add some tags

--- internal/screenshot/action/crawlpic.go
+++ internal/screenshot/action/crawlpic.go
@@ -7,29 +7,32 @@ import (
 )
 
 // NetworkTrafficAction: 进入指定线路，选取指定时间内的流量状况
-func NetworkTrafficAction(url string, timeRange, sleepTime int) chromedp.Tasks {
+func NetworkTrafficAction(url string, timeRange string, sleepTime int) chromedp.Tasks {
 	var tasks chromedp.Tasks
 	tasks = append(tasks, chromedp.Navigate(url))
 	switch timeRange {
-	case 1:
+	case "1h":
 		oneHour := `#tab_1 > div.time-quick-range > div:nth-child(4) > ul > li:nth-child(4) > a`
 		tasks = append(tasks, chromedp.Click(oneHour, chromedp.NodeVisible))
-	case 3:
+	case "3h":
 		threeHours := `#tab_1 > div.time-quick-range > div:nth-child(4) > ul > li:nth-child(5) > a`
 
 		tasks = append(tasks, chromedp.Click(threeHours, chromedp.NodeVisible))
-	case 6:
+	case "6h":
 		sixHours := `#tab_1 > div.time-quick-range > div:nth-child(4) > ul > li:nth-child(6) > a`
 		tasks = append(tasks, chromedp.Click(sixHours, chromedp.NodeVisible))
-	case 12:
+	case "12h":
 		twelveHours := `#tab_1 > div.time-quick-range > div:nth-child(4) > ul > li:nth-child(7) > a`
 		tasks = append(tasks, chromedp.Click(twelveHours, chromedp.NodeVisible))
-	case 24:
+	case "24h":
 		twentyFourHours := `#tab_1 > div.time-quick-range > div:nth-child(4) > ul > li:nth-child(8) > a`
 		tasks = append(tasks, chromedp.Click(twentyFourHours, chromedp.NodeVisible))
-	case 15:
+	case "15m":
 		m15 := `#tab_1 > div.time-quick-range > div:nth-child(4) > ul > li:nth-child(2) > a`
 		tasks = append(tasks, chromedp.Click(m15, chromedp.NodeVisible))
+	case "30m":
+		m30 := `#tab_1 > div.time-quick-range > div:nth-child(4) > ul > li:nth-child(3) > a`
+		tasks = append(tasks, chromedp.Click(m30, chromedp.NodeVisible))
 	}
 	tasks = append(tasks, chromedp.Sleep(time.Duration(sleepTime)*time.Millisecond))
 	// tasks = append(tasks, chromedp.WaitVisible(`#graph_full`, chromedp.ByID))
