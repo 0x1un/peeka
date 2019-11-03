9788b6ed4fa3864fcbcc054994adf12c8c91d5e7
aumujun <aumujun@gmail.com>
2019-11-03 15:49:13 +0800

    .

--- cmd/screenshot/main.go
+++ cmd/screenshot/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"os"
+
+	"peeka/internal/chatbot"
 	"peeka/internal/screenshot/run"
 )
 
@@ -15,5 +18,5 @@ func main() {
 		buffer.WriteString("![" + k + "]" + "(" + v + ")\n")
 	}
 	fmt.Println(buffer.String())
-	// chatbot.Run(os.Getenv("ROBOT_TOKEN"), "ALL", buffer.String())
+	chatbot.Run(os.Getenv("ROBOT_TOKEN"), "ALL", buffer.String())
 }
