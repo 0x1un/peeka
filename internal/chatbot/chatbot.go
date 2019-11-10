9720df0064ec67f72f1b355ef1ff6a6e316dd2e6
aumujun <aumujun@gmail.com>
2019-11-10 23:44:20 +0800

    refactor chatbot code

--- internal/chatbot/chatbot.go
+++ internal/chatbot/chatbot.go
@@ -2,58 +2,54 @@ package chatbot
 
 import (
 	"bytes"
-	"io/ioutil"
+	"encoding/json"
 	"log"
-	"net/http"
 	"os"
+	"peeka/internal/component"
 	"time"
 )
 
-var (
+const (
 	baseURL = `https://oapi.dingtalk.com/robot/send?access_token=`
 )
 
-func Run(token, user, text string) {
+type Message struct {
+	MsgType  string `json:"msgtype"`
+	Markdown struct {
+		Title string `json:"title"`
+		Text  string `json:"text"`
+	} `json:"markdown"`
+	At struct {
+		AtMobiles []string `json:"atMobiles"`
+		IsAtAll   bool     `json:"isAtAll"`
+	} `json:"at"`
+}
+
+func Send(tokens, atUsers []string, notifyAll bool, text string) {
 	fileName := time.Now().Format("20060102") + ".dingding.log"
 	logFile, _ := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
 	defer logFile.Close()
 	Log := log.New(logFile, "[Info]", log.Ldate|log.Ltime) // log.Ldate|log.Ltime|log.Lshortfile
 	Log.Println("开始发送消息!")
-	SendMsg(token, user, text, Log)
+	msg := new(Message)
+	msg.MsgType = "markdown"
+	msg.Markdown.Title = "[钉钉红包]恭喜发财 大吉大利!"
+	msg.Markdown.Text = text
+	msg.At.AtMobiles = atUsers
+	msg.At.IsAtAll = notifyAll
+	msgs, err := json.Marshal(msg)
+	if err != nil {
+		Log.Fatal(err)
+	}
+	for _, tk := range tokens {
+		fillMsgAndSent(tk, msgs, Log)
+	}
 }
 
 //发送消息到钉钉
-func SendMsg(token, user, text string, Log *log.Logger) {
-	jsonstring := `{
-     "msgtype": "markdown",
-     "markdown": {"title":"皮卡丘!",
-"text":"` + text + `"
-     },
-    "at": {
-        "atMobiles": [
-            "` + user + `"
-        ], 
-        "isAtAll": true
-    }
- }`
-	reader := bytes.NewReader([]byte(jsonstring))
-	resp := Post(baseURL+token, reader)
+func fillMsgAndSent(token string, msg []byte, Log *log.Logger) {
+	reader := bytes.NewReader(msg)
+	resp := component.Post(baseURL+token, reader)
 	Log.SetPrefix("[Info]")
 	Log.Printf("消息发送完成,服务器返回内容：%s", string(resp))
-}
-
-func Post(url string, reader *bytes.Reader) []byte {
-	request, err := http.NewRequest("POST", url, reader)
-	if err != nil {
-
-	}
-	request.Header.Set("Content-Type", "application/json")
-	client := http.Client{}
-	resp, err := client.Do(request)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	defer resp.Body.Close()
-	respBytes, _ := ioutil.ReadAll(resp.Body)
-	return respBytes
 }
