a7e7a9b2f73d825dca3647520ceb68dd4714fe58
0x1un <aumujun@gmail.com>
2019-11-13 17:01:45 +0800

    .

--- cmd/dingtalk/main.go
+++ cmd/dingtalk/main.go
@@ -14,16 +14,12 @@ var (
 
 func main() {
 	client := api.Client
-	// userids := []string{
-	// 	"2749481918775803",
-	// }
-
-	// result, err := client.GetListRecord(userids, "2019-10-28 08:00:00", "2019-10-29 00:00:00", 0, 50)
-	// result, err := client.GetUserInfoDetailsByUid(userids[0], "")
-	// result, err := client.GetUserIdByMobile("17608035126")
-	result, err := client.GetUsersOfDepartmentByDepId("105372678", "0", "100", "")
+	userids := []string{
+		"2749481918775803",
+	}
+	data, err := client.GetListRecord(userids, "2019-11-11 08:00:00", "2019-11-12 00:00:00", 0, 20)
 	if err != nil {
 		log.Println(err)
 	}
-	fmt.Println(result)
+	fmt.Println(data)
 }
