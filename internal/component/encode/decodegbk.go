3aa53317b6cece62270d9aaf752986092421f798
0x1un <aumujun@gmail.com>
2019-12-09 18:15:17 +0800

    gbk decoder

---
+++ internal/component/encode/decodegbk.go
@@ -0,0 +1,19 @@
+package encode
+
+import (
+	"bytes"
+	"io/ioutil"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+)
+
+func DecodeGBK(s []byte) ([]byte, error) {
+	I := bytes.NewReader(s)
+	O := transform.NewReader(I, simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(O)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
